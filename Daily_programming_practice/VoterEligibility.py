549fde3a0360faa5e9016f471aba296efd56a1ca
Arun G Nayak <nayakarun02@gmail.com>
2022-07-02 21:28:41 +0530

    added voting

---
+++ Daily_programming_practice/VoterEligibility.py
@@ -0,0 +1,11 @@
+
+# This program takes age as input and prints whether he is eligible or not
+
+age = int(input("Enter the age of the voter : "))
+
+if age >= 18:
+    print("This person is eligible for vote")
+else:
+    print("This person is not eligible for vote")
+
+
