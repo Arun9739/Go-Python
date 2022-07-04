b774ca24b6ec00e5f5855d7a47f999844a625247
Arun G Nayak <nayakarun02@gmail.com>
2022-07-04 22:04:21 +0530

    Added larger

---
+++ Daily_programming_practice/FindLargest.py
@@ -0,0 +1,10 @@
+
+# This program calculates the largest number between two numbers
+
+first = int(input("Enter the first number : "))
+second = int(input("Enter the second number : "))
+
+if first > second:
+    print(str(first) + " is larger than " + str(second))
+else:
+    print(str(second) + " is larger than "  + str(first))
