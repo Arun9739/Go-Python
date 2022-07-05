d9620c26cd06393249212915ec4a29716a7d208a
Arun G Nayak <nayakarun02@gmail.com>
2022-07-05 21:15:23 +0530

    added minimum code

---
+++ Daily_programming_practice/FindSmallest.py
@@ -0,0 +1,11 @@
+
+# This program prints the minimum of two given numbers
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+
+if num1 < num2:
+    print(str(num1) + " is lesser than " + str(num2))
+else:
+    print(str(num2) + " is lesser than " + str(num1))
+
