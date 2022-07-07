2546416241e9271e606693810d610f496795aa2e
Arun G Nayak <nayakarun02@gmail.com>
2022-07-07 23:31:54 +0530

    added remainder code

---
+++ Daily_programming_practice/RemainderOfTwoNums.py
@@ -0,0 +1,6 @@
+
+a = int(input("Enter the number to be divided : "))
+b = int(input("Enter the number which divides : "))
+
+print("\nThe quotient is " + str(a // b))
+print("The remainder is " + str(a % b))
