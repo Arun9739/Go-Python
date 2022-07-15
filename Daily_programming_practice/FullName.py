4e65488cf69c4416a688021fdba944c7258770b3
Arun G Nayak <nayakarun02@gmail.com>
2022-07-15 21:16:25 +0530

    Create FullName.py

---
+++ Daily_programming_practice/FullName.py
@@ -0,0 +1,8 @@
+
+# This program prints the full name of a person by taking his first, middle and last names
+
+fName = input("Enter first name : ")
+mName = input("Enter middle name : ")
+lName = input("Enter last name : ")
+
+print("The full name is " + fName + " " + mName + " " + lName)
