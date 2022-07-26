6032b3cd00ba47141e626b7097958db5a36c40c4
Arun G Nayak <nayakarun02@gmail.com>
2022-07-26 21:29:52 +0530

    Create PrintNumbers.py

---
+++ Daily_programming_practice/PrintNumbers.py
@@ -0,0 +1,10 @@
+
+# This program takes an input 'N' from user and prints 'N' natural numbers
+
+
+n = int(input("Enter the number : "))
+
+print("The numbers are : ")
+
+for i in range(1, n + 1):
+    print(i, end = " ")
