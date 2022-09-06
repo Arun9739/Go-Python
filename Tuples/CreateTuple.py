3559ddc5c54921f5599e784b39811dd4346df60b
Arun G Nayak <nayakarun02@gmail.com>
2022-09-06 21:20:09 +0530

    Create CreateTuple.py

---
+++ Tuples/CreateTuple.py
@@ -0,0 +1,9 @@
+
+# Program to create a Tuple in python
+
+teams = ("Chennai Super Kings", "Royal Challengers Bangalore", 2023, 16.0, "Mumbai Indians")
+
+print("Items in Tuples : ")
+
+for item in teams:
+    print(item)
