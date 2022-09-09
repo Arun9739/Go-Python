201dba521840b37d390b67d0c8a116682766e40f
Arun G Nayak <nayakarun02@gmail.com>
2022-09-09 21:39:14 +0530

    Create Create_Dictionary.py

---
+++ Dictionary/Create_Dictionary.py
@@ -0,0 +1,8 @@
+
+teams = {"CSK": "Chennai Super Kings",
+         "RCB": "Royal Challengers Bangalore",
+         "MI": "Mumbai Indians"}
+
+
+for code in teams:
+    print(code + " : " + teams[code])
