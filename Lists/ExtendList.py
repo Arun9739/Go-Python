4c4333aafddfc66429a1aa02af4b134b093da8f8
Arun G Nayak <nayakarun02@gmail.com>
2022-12-11 21:29:28 +0530

    Create ExtendList.py

---
+++ Lists/ExtendList.py
@@ -0,0 +1,12 @@
+
+# Python program to demonstrate the extend function in lists
+
+positives = [1, 2, 3]
+negatives = [4, 5, 6]
+
+print("Positive numbrs : ", positives)
+print("Negative numbers : ", negatives)
+
+positives.extend(negatives)
+
+print("Extended positives : ", positives)
